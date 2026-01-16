0a70817b216eecd6736031b0329688a5b8115962
COMRADE040 <rafiulislam040@gmail.com>
2026-01-16 23:22:48 +0600

    navlink in

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,14 @@
 import Title from '@/components/Title';
+import Link from 'next/link';
 import React from 'react';
 
 const About = () => {
     return (
         <div>
             <Title>Welcome to About</Title>
+            <nav className='space-x -5'>
+                <Link href="/about/contact">Contact</Link>
+            </nav>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
         
-             <header className="px-4 py-3 border-b-2 border-gray-500">
-        <h2 className="text-lg font-semibold">  Dev-Story</h2>
+             <header className="px-4 py-3 border-b-2 border-gray-500 flex justify-between items-center">
+        <Link href='/' className="text-lg font-semibold">  Dev-Story</Link>
+
+
+<nav className='space-x-20'>
+    <Link href="/about">About</Link>
+    <Link href="/about/contact">Contact</Link>
+    <Link href="/about/teampage/teammember">Team Member</Link>
+    <Link href="/about/teampage">Teams</Link>
+    <Link href="/login">Login</Link>
+    <Link href="/register">Register</Link>
+    <Link href="/tutorials">Tutorials</Link>
+</nav>
+
       </header>
        
     );
